0275404902996f0c100ded21b966818cc1a455b4
aarv0 <42588932+aarv0@users.noreply.github.com>
2019-03-14 20:48:07 -0600

    Create swpeople.js

---
+++ swpeople.js
@@ -0,0 +1,21 @@
+import { people } from'../people.js'
+
+const men = people.filter(person => person.gender === 'male')
+const women = people.filter(person => person.gender === 'female')
+const other = people.filter(person => (person.gender === 'n/a') || (person.gender === 'hermaphrodite') || (person.gender === 'none'))
+
+//console.log(men, women, other)
+
+const mainContainer = document.createElement('div')
+mainContainer.className = 'container'
+
+men.forEach((man) => {
+  let manElement = document.createElement('div)
+  manElement.className = 'box'
+  manElement.textContent = man.name
+  mainContainer.appendChild(mainContainer)
+  })
+
+const mainContainer = document.createElement('div')
+mainContainer.className = 'container'
+
